cee7d8df8cf3271d7cb99858b7b8a2df02e425ac
arturican <arturican@gmail.com>
2022-11-23 18:16:14 +0300

    add xtype

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,9 +19,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button
         + (disabled ? '' + s.disabled: '' + s.button)
-        // ? : xType === 'red'
+        +(xType  ? s.default : xType === 'red')
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (className ? ' ' +s.className : '') // задачка на смешивание классов
 
     return (
         <button
